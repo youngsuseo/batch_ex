a828f6de8efc65b2043136874fb36ef3ec683c61
youngsuseo <56705264+youngsuseo@users.noreply.github.com>
2022-12-11 11:32:12 +0900

    Initial commit

---
+++ src/main/java/com/example/batch_ex/BatchExApplication.java
@@ -0,0 +1,13 @@
+package com.example.batch_ex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchExApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BatchExApplication.class, args);
+    }
+
+}
